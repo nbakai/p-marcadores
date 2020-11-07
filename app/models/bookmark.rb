04130d59f53998d22d83973848314f67ca2e3136
Melisa Budini <melixbq@gmail.com>
2020-11-07 04:07:06 -0300

    up

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ApplicationRecord
   belongs_to :tipo
-  has_and_belongs_to_many :categories, dependent: :destroy
-  has_many :subcategories, dependent: :destroy
+  has_and_belongs_to_many :categories
+  has_many :subcategories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   post 'bookmarks/:id/add_category', to: 'bookmarks#add_category', as: 'add_category'
   post 'bookmarks/:id/add_subcategory', to: 'bookmarks#add_subcategory', as: 'add_subcategory'
   delete 'bookmarks/:id/remove_category/:category_id', to: 'bookmarks#remove_category', as: 'remove_category'
-  root 'bookmarks#index'
   get 'subcategories', to: 'subcategories#index', as: 'all_subcategories'
   get 'subcategories/new', to: 'subcategories#new', as: 'new_subcategorie'
   post 'subcategories', to: 'subcategories#create', as: 'subcategories'
+  root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
